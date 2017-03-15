2c410ce8f373dc6bd862b57ddc1bc160e448b50c
MCocirio <marianococirio@gmail.com>
2017-03-15 15:29:47 -0300

    backend listo

--- mongooseTasks/express.js
+++ mongooseTasks/express.js
@@ -31,12 +31,40 @@ var Task = mongoose.model('Task', taskSchema);
 
 //Posting tasks
 app.post('/tasks', function(req, res){
-    var t = new Task(req.body);
+    var t = new Task({
+      name: req.body.name,
+      user: req.body.name,
+      status: req.body.status,
+      start: req.body.start
+  });
     t.save(function(err,t) {
         if (err) return res.status(500).send(err);
         res.status(201).send(t);
     });
 });
+
+//Modify a task
+
+app.put('/tasks', function (req, res){
+  Task.findById(req.body._id, function(err, p) {
+    console.log(p);
+  if (!p)
+    return res.status(500).send(err);
+  else {
+        p.name = req.body.name;
+        p.user = req.body.user;
+        p.status = req.body.status;
+        p.start = req.body.start;
+        p.save(function(err,p) {
+          if (err)
+            res.status(500).send(err);
+          else
+            res.status(201).send(p);
+        });
+      }
+  });
+});
+
 
 //Getting all tasks
 app.get('/tasks', function (req, res) {
