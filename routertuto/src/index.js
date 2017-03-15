99d8b6e4971cae77e88f5108e71334c431d0b79c
MCocirio <marianococirio@gmail.com>
2017-03-15 17:56:03 -0300

    ejemplo basico react router

---
+++ routertuto/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BasicExample from './App';
+import './index.css';
+
+ReactDOM.render(
+  <BasicExample />,
+  document.getElementById('root')
+);
