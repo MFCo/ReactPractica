c4f16661446c72e117c465d24f4bb98f66a8c878
MCocirio <marianococirio@gmail.com>
2017-05-03 17:24:12 -0300

    plugin catch marianoStyle and generate css string

--- sample-babel/sample.js
+++ sample-babel/sample.js
@@ -1,20 +1,41 @@
 class Pija extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log("mounted");
     }
-    
-    marianoStyle(){
+
+    marianoStyle() {
         color = red;
         position = relative;
     }
 
-    render(){
+    render() {
         return
-            //<div> HOLA </div>
-        
+        <div className={""}> HOLA </div>
+
+    }
+}
+
+class Asde extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log("mounted");
+    }
+
+    marianoStyle() {
+        color = blue;
+        backgroundColor = relative;
+    }
+
+    render() {
+        return
+        <div className={""}> HOLA </div>
+
     }
 }
